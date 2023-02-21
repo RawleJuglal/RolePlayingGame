666ba156dda8edb3e20177a48b0dea41e1cac4a7
Rawle Juglal <RawleJuglal@users.noreply.github.com>
2023-02-21 00:27:11 -0600

    feature added
    renderCharacter function

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 import './style.css'
+
+const hero = {
+  elementId: "hero",
+  name: `Wizard`,
+  avatar: `images/wizard.png`,
+  health:60,
+  diceRoll:6 
+}
+
+const monster = {
+  elementId:"monster",
+  name:`Orc`,
+  avatar: `images/orc.png`,
+  health:10,
+  diceRoll:4   
+}
+
+function renderCharacter(data) {
+  const { elementId, name, avatar, health, diceRoll} = data
+  document.getElementById(elementId).innerHTML =
+      `<div class="character-card">
+          <h4 class="name"> ${name} </h4>
+          <img class="avatar" src="${avatar}" />
+          <div class="health">health: <b> ${health} </b></div>
+          <div class="dice-container">
+              <div class="dice"> ${diceRoll} </div>
+          </div>
+      </div>`
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
