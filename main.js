290ae0efb202918a3217b02b9fb67bdb9dcdd26a
Rawle Juglal <rawledjuglal@gmail.com>
2023-02-21 18:38:01 -0600

    completed section 1
    Setup - hero/monster objects with functions to render random dice rolls underneath

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const hero = {
   name: `Wizard`,
   avatar: `images/wizard.png`,
   health:60,
-  diceRoll:6 
+  diceCount:3 
 }
 
 const monster = {
@@ -13,18 +13,36 @@ const monster = {
   name:`Orc`,
   avatar: `images/orc.png`,
   health:10,
-  diceRoll:4   
+  diceCount:1,   
+}
+
+function getRollHtml(rollCount){
+  return getDiceRollArray(rollCount).map((die)=>{
+    return `<div class="dice">${die}</div>`
+  }).join('')
+}
+
+function getDiceRollArray(count){
+  return new Array(count).fill(0).map(()=>{
+    return getRandomDice()
+  });
+}
+
+function getRandomDice(){
+  return Math.floor((Math.random()*6)+1)
 }
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll} = data
+  const { elementId, name, avatar, health, diceCount} = data
+  const diceHtml = getRollHtml(diceCount)
+
   document.getElementById(elementId).innerHTML =
       `<div class="character-card">
           <h4 class="name"> ${name} </h4>
           <img class="avatar" src="${avatar}" />
           <div class="health">health: <b> ${health} </b></div>
           <div class="dice-container">
-              <div class="dice"> ${diceRoll} </div>
+              ${diceHtml}
           </div>
       </div>`
 }
